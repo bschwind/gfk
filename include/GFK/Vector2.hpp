674e0617a364637ba6e301b98b1e028473615116
Brian Schwind <brianmschwind@gmail.com>
2013-04-02 09:20:15 -0400

    Adding more methods to Vector2

--- include/GFK/Vector2.hpp
+++ include/GFK/Vector2.hpp
@@ -12,6 +12,9 @@ public:
 	~Vector2();
 
 	static float Dot(const Vector2 &v1, const Vector2 &v2);
+	static float Length(const Vector2 &v);
+	static void Rotate(Vector2 &v, float angle);
+	static void Normalize(Vector2 &v);
 protected:
 private:
 };
--- src/GFK/Vector2.cpp
+++ src/GFK/Vector2.cpp
@@ -1,4 +1,5 @@
 #include <GFK/Vector2.hpp>
+#include <cmath>
 
 namespace gfk
 {
@@ -22,5 +23,27 @@ namespace gfk
 	float Vector2::Dot(const Vector2 &v1, const Vector2 &v2)
 	{
 		return v1.X * v2.X + v1.Y * v2.Y;
+	}
+
+	float Vector2::Length(const Vector2 &v)
+	{
+		return sqrt(v.X * v.X + v.Y * v.Y);
+	}
+
+	void Vector2::Rotate(Vector2 &v, float angle)
+	{
+		float cosVal = cos(angle);
+		float sinVal = sin(angle);
+
+		float temp = v.X;
+		v.X = cosVal * v.X - sinVal * v.Y;
+		v.Y = sinVal * temp + cosVal * v.Y;
+	}
+
+	void Vector2::Normalize(Vector2 &v)
+	{
+		float length = sqrt(v.X * v.X + v.Y * v.Y);
+		v.X /= length;
+		v.Y /= length;
 	}
 }
