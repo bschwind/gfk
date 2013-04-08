be7791c8b5d7528e25da9d5d2bfa10f1e33063ad
Brian Schwind <brianmschwind@gmail.com>
2013-04-08 09:31:11 -0400

    Added a MathHelper class

---
+++ include/GFK/MathHelper.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+namespace gfk
+{
+
+class MathHelper
+{
+public:
+	static inline float Barycentric(float v1, float v2, float v3, float amount1, float amount2)
+	{
+		return v1 + (v2 - v1) * amount1 + (v3 - v1) * amount2;
+	}
+
+	static inline float CatmullRom(float v1, float v2, float v3, float v4, float amount)
+	{
+		// Using formula from http://www.mvps.org/directx/articles/catmull/
+		// Internally using doubles not to lose precission
+		double amountSquared = amount * amount;
+		double amountCubed = amountSquared * amount;
+		return (float)(0.5 * (2.0 * v2 +
+		(v3 - v1) * amount +
+		(2.0 * v1 - 5.0 * v2 + 4.0 * v3 - v4) * amountSquared +
+		(3.0 * v2 - v1 - 3.0 * v3 + v4) * amountCubed));
+
+	}
+
+	static inline float Clamp(const float value, const float min, const float max)
+	{
+		return value < min ? min : (value > max ? max : value);
+	}
+
+	static inline void Clamp(float &value, const float min, const float max)
+	{
+		value = (value < min) ? min : (value > max ? max : value);
+	}
+
+	static float Hermite(float v1, float tangent1, float v2, float tangent2, float amount);
+};
+
+}
---
+++ src/GFK/MathHelper.cpp
@@ -0,0 +1,30 @@
+#include <GFK/MathHelper.hpp>
+
+namespace gfk
+{
+	float MathHelper::Hermite(float v1, float tangent1, float v2, float tangent2, float amount)
+	{
+		// All transformed to double not to lose precission
+		// Otherwise, for high numbers of param:amount the result is NaN instead of Infinity
+		double d1 = v1, d2 = v2, t1 = tangent1, t2 = tangent2, s = amount, result;
+		double sSquared = s * s;
+		double sCubed = s * sSquared;
+
+		if (amount == 0.0f)
+		{
+			result = v1;
+		}
+		else if (amount == 1.0f)
+		{
+			result = v2;
+		}
+		else
+		{
+			result = (2 * d1 - 2 * d2 + t2 + t1) * sCubed +
+			(3 * d2 - 3 * d1 - 2 * t1 - t2) * sSquared +
+			t1 * s + d1;
+		}
+
+		return (float)result;
+	}
+}
