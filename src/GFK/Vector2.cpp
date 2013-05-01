1a3d51e1986ca16e956a6f2dce232346ffd4f278
Brian Schwind <brianmschwind@gmail.com>
2013-05-01 14:46:16 -0400

    Updated Vector2 to follow the style of XNA more closely

--- src/GFK/Vector2.cpp
+++ src/GFK/Vector2.cpp
@@ -26,7 +26,7 @@ namespace gfk
 	const Vector2 Vector2::UnitX(1.0f, 0.0f);
 	const Vector2 Vector2::UnitY(0.0f, 1.0f);
 
-	Vector2 Vector2::Barycentric(Vector2 &v1, Vector2 &v2, Vector2 &v3, float amount1, float amount2)
+	Vector2 Vector2::Barycentric(const Vector2 &v1, const Vector2 &v2, const Vector2 &v3, float amount1, float amount2)
 	{
 		return Vector2(
 			MathHelper::Barycentric(v1.X, v2.X, v3.X, amount1, amount2),
@@ -34,11 +34,23 @@ namespace gfk
 
 	}
 
-	Vector2 Vector2::CatmullRom(Vector2 &v1, Vector2 &v2, Vector2 &v3, Vector2 &v4, float amount)
+	void Vector2::Barycentric(const Vector2 &v1, const Vector2 &v2, const Vector2 &v3, float amount1, float amount2, Vector2 &result)
+	{
+		result.X = MathHelper::Barycentric(v1.X, v2.X, v3.X, amount1, amount2);
+		result.Y = MathHelper::Barycentric(v1.Y, v2.Y, v3.Y, amount1, amount2);
+	}
+
+	Vector2 Vector2::CatmullRom(const Vector2 &v1, const Vector2 &v2, const Vector2 &v3, Vector2 &v4, float amount)
 	{
 		return Vector2(
 			MathHelper::CatmullRom(v1.X, v2.X, v3.X, v4.X, amount),
 			MathHelper::CatmullRom(v1.Y, v2.Y, v3.Y, v4.Y, amount));
+	}
+
+	void Vector2::CatmullRom(const Vector2 &v1, const Vector2 &v2, const Vector2 &v3, Vector2 &v4, float amount, Vector2 &result)
+	{
+		result.X = MathHelper::CatmullRom(v1.X, v2.X, v3.X, v4.X, amount);
+		result.Y = MathHelper::CatmullRom(v1.Y, v2.Y, v3.Y, v4.Y, amount);
 	}
 
 	Vector2 Vector2::Clamp(const Vector2 &v, const Vector2 &min, const Vector2 &max)
@@ -48,11 +60,24 @@ namespace gfk
 			MathHelper::Clamp(v.Y, min.Y, max.Y));
 	}
 
+	void Vector2::Clamp(const Vector2 &v, const Vector2 &min, const Vector2 &max, Vector2 &result)
+	{
+		result.X = MathHelper::Clamp(v.X, min.X, max.X);
+		result.Y = MathHelper::Clamp(v.Y, min.Y, max.Y);
+	}
+
 	float Vector2::Distance(const Vector2 &v1, const Vector2 &v2)
 	{
 		float x = v1.X - v2.X;
 		float y = v1.Y - v2.Y;
 		return sqrt(x * x + y * y);
+	}
+
+	void Vector2::Distance(const Vector2 &v1, const Vector2 &v2, float &result)
+	{
+		float x = v1.X - v2.X;
+		float y = v1.Y - v2.Y;
+		result = sqrt(x * x + y * y);
 	}
 
 	float Vector2::DistanceSquared(const Vector2 &v1, const Vector2 &v2)
@@ -62,26 +87,34 @@ namespace gfk
 		return x * x + y * y;
 	}
 
+	void Vector2::DistanceSquared(const Vector2 &v1, const Vector2 &v2, float &result)
+	{
+		float x = v1.X - v2.X;
+		float y = v1.Y - v2.Y;
+		result =  x * x + y * y;
+	}
+
 	float Vector2::Dot(const Vector2 &v1, const Vector2 &v2)
 	{
 		return v1.X * v2.X + v1.Y * v2.Y;
 	}
 
-	Vector2 Vector2::Hermite(Vector2 &v1, Vector2 &tangent1, Vector2 &v2, Vector2 &tangent2, float amount)
+	void Vector2::Dot(const Vector2 &v1, const Vector2 &v2, float &result)
+	{
+		result = v1.X * v2.X + v1.Y * v2.Y;
+	}
+
+	Vector2 Vector2::Hermite(const Vector2 &v1, const Vector2 &tangent1, const Vector2 &v2, const Vector2 &tangent2, float amount)
 	{
 		return Vector2(
 			MathHelper::Hermite(v1.X, tangent1.X, v2.X, tangent2.X, amount), 
 			MathHelper::Hermite(v1.Y, tangent1.Y, v2.Y, tangent2.Y, amount));
 	}
 
-	float Vector2::Length(const Vector2 &v)
+	void Vector2::Hermite(const Vector2 &v1, const Vector2 &tangent1, const Vector2 &v2, const Vector2 &tangent2, float amount, Vector2 &result)
 	{
-		return sqrt(v.X * v.X + v.Y * v.Y);
-	}
-
-	float Vector2::LengthSquared(const Vector2 &v)
-	{
-		return v.X * v.X + v.Y * v.Y;
+		result.X = MathHelper::Hermite(v1.X, tangent1.X, v2.X, tangent2.X, amount);
+		result.Y = MathHelper::Hermite(v1.Y, tangent1.Y, v2.Y, tangent2.Y, amount);
 	}
 
 	Vector2 Vector2::Lerp(const Vector2 &v1, const Vector2 &v2, float amount)
@@ -91,6 +124,12 @@ namespace gfk
 			MathHelper::Lerp(v1.Y, v2.Y, amount));
 	}
 
+	void Vector2::Lerp(const Vector2 &v1, const Vector2 &v2, float amount, Vector2 &result)
+	{
+		result.X = MathHelper::Lerp(v1.X, v2.X, amount);
+		result.Y = MathHelper::Lerp(v1.Y, v2.Y, amount);
+	}
+
 	Vector2 Vector2::Max(const Vector2 &v1, const Vector2 &v2)
 	{
 		return Vector2(
@@ -98,10 +137,23 @@ namespace gfk
 			v1.Y > v2.Y ? v1.Y : v2.Y);
 	}
 
+	void Vector2::Max(const Vector2 &v1, const Vector2 &v2, Vector2 &result)
+	{
+		result.X = v1.X > v2.X ? v1.X : v2.X;
+		result.Y = v1.Y > v2.Y ? v1.Y : v2.Y;
+	}
+
 	Vector2 Vector2::Min(const Vector2 &v1, const Vector2 &v2)
 	{
-		return Vector2(v1.X < v2.X ? v1.X : v2.X,
+		return Vector2(
+			v1.X < v2.X ? v1.X : v2.X,
 			v1.Y < v2.Y ? v1.Y : v2.Y);
+	}
+
+	void Vector2::Min(const Vector2 &v1, const Vector2 &v2, Vector2 &result)
+	{
+		result.X = v1.X < v2.X ? v1.X : v2.X;
+		result.Y = v1.Y < v2.Y ? v1.Y : v2.Y;
 	}
 
 	Vector2 Vector2::Negate(Vector2 &v)
@@ -111,29 +163,27 @@ namespace gfk
 		return v;
 	}
 
-	Vector2 Vector2::Negate(const Vector2 &v)
+	void Vector2::Negate(const Vector2 &v, Vector2 &result)
 	{
-		Vector2 vOut(v.X, v.Y);
-		vOut.X = -vOut.X;
-		vOut.Y = -vOut.Y;
-		return vOut;
+		result.X = -v.X;
+		result.Y = -v.Y;
 	}
 
 	Vector2 Vector2::Normalize(Vector2 &v)
 	{
-		float length = sqrt(v.X * v.X + v.Y * v.Y);
-		v.X /= length;
-		v.Y /= length;
+		float val = 1.0f / sqrt(v.X * v.X + v.Y * v.Y);
+		v.X *= val;
+		v.Y *= val;
 		return v;
 	}
 
-	Vector2 Vector2::Normalize(const Vector2 &v)
+	void Vector2::Normalize(const Vector2 &v, Vector2 &result)
 	{
-		Vector2 vOut(v.X, v.Y);
-		float length = sqrt(vOut.X * vOut.X + vOut.Y * vOut.Y);
-		vOut.X /= length;
-		vOut.Y /= length;
-		return vOut;
+		result.X = v.X;
+		result.Y = v.Y;
+		float val = 1.0f / sqrt(result.X * result.X + result.Y * result.Y);
+		result.X *= val;
+		result.Y *= val;
 	}
 
 	Vector2 Vector2::Reflect(const Vector2 &v, const Vector2 &normal)
@@ -145,21 +195,60 @@ namespace gfk
 		return result;
 	}
 
-	void Vector2::Rotate(Vector2 &v, float angle)
+	void Vector2::Reflect(const Vector2 &v, const Vector2 &normal, Vector2 &result)
+	{
+		float val = 2.0f * ((v.X * normal.X) + (v.Y * normal.Y));
+		result.X = v.X - (normal.X * val);
+		result.Y = v.Y - (normal.Y * val);
+	}
+
+	Vector2 Vector2::Rotate(const Vector2 &v, float angle)
 	{
 		float cosVal = cos(angle);
 		float sinVal = sin(angle);
 
-		float temp = v.X;
-		v.X = cosVal * v.X - sinVal * v.Y;
-		v.Y = sinVal * temp + cosVal * v.Y;
+		return Vector2(
+			cosVal * v.X - sinVal * v.Y,
+			sinVal * v.X + cosVal * v.Y);
 	}
 
-	Vector2 SmoothStep(Vector2 &v1, Vector2 &v2, float amount)
+	void Vector2::Rotate(const Vector2 &v, float angle, Vector2 &result)
+	{
+		float cosVal = cos(angle);
+		float sinVal = sin(angle);
+
+		result.X = cosVal * v.X - sinVal * v.Y;
+		result.Y = sinVal * v.X + cosVal * v.Y;
+	}
+
+	Vector2 Vector2::SmoothStep(const Vector2 &v1, const Vector2 &v2, float amount)
 	{
 		return Vector2(
 			MathHelper::SmoothStep(v1.X, v2.X, amount),
 			MathHelper::SmoothStep(v1.Y, v2.Y, amount));
+	}
+
+	void Vector2::SmoothStep(const Vector2 &v1, const Vector2 &v2, float amount, Vector2 &result)
+	{
+		result.X = MathHelper::SmoothStep(v1.X, v2.X, amount);
+		result.Y = MathHelper::SmoothStep(v1.Y, v2.Y, amount);
+	}
+
+	float Vector2::Length()
+	{
+		return sqrt(X * X + Y * Y);
+	}
+
+	float Vector2::LengthSquared()
+	{
+		return X * X + Y * Y;
+	}
+
+	void Vector2::Normalize()
+	{
+		float val = 1.0f / sqrt(X * X + Y * Y);
+		X *= val;
+		Y *= val;
 	}
 
 	Vector2 operator+ (const Vector2 &op1, const Vector2 &op2)
