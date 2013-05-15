e54c166e326f90b8410f56f214c33cc70330a5ce
Brian Schwind <brianmschwind@gmail.com>
2013-05-15 13:22:45 -0400

    Formatted to be consistent with the other CPP files

--- src/GFK/Math/MathHelper.cpp
+++ src/GFK/Math/MathHelper.cpp
@@ -2,34 +2,36 @@
 
 namespace gfk
 {
-	float MathHelper::Hermite(float v1, float tangent1, float v2, float tangent2, float amount)
+
+float MathHelper::Hermite(float v1, float tangent1, float v2, float tangent2, float amount)
+{
+	double d1 = v1, d2 = v2, t1 = tangent1, t2 = tangent2, s = amount, result;
+	double sSquared = s * s;
+	double sCubed = s * sSquared;
+
+	if (amount == 0.0f)
 	{
-		double d1 = v1, d2 = v2, t1 = tangent1, t2 = tangent2, s = amount, result;
-		double sSquared = s * s;
-		double sCubed = s * sSquared;
-
-		if (amount == 0.0f)
-		{
-			result = v1;
-		}
-		else if (amount == 1.0f)
-		{
-			result = v2;
-		}
-		else
-		{
-			result = (2 * d1 - 2 * d2 + t2 + t1) * sCubed +
-			(3 * d2 - 3 * d1 - 2 * t1 - t2) * sSquared +
-			t1 * s + d1;
-		}
-
-		return (float)result;
+		result = v1;
+	}
+	else if (amount == 1.0f)
+	{
+		result = v2;
+	}
+	else
+	{
+		result = (2 * d1 - 2 * d2 + t2 + t1) * sCubed +
+		(3 * d2 - 3 * d1 - 2 * t1 - t2) * sSquared +
+		t1 * s + d1;
 	}
 
-	float MathHelper::SmoothStep(float v1, float v2, float amount)
-	{
-		float result = MathHelper::Clamp(amount, 0.0f, 1.0f);
-		result = MathHelper::Hermite(v1, 0.0f, v2, 0.0f, result);
-		return result;
-	}
+	return (float)result;
+}
+
+float MathHelper::SmoothStep(float v1, float v2, float amount)
+{
+	float result = MathHelper::Clamp(amount, 0.0f, 1.0f);
+	result = MathHelper::Hermite(v1, 0.0f, v2, 0.0f, result);
+	return result;
+}
+
 }
--- src/GFK/Math/Matrix.cpp
+++ src/GFK/Math/Matrix.cpp
@@ -21,44 +21,46 @@
 
 namespace gfk
 {
-	Matrix::Matrix()
-	{
 
-	}
+Matrix::Matrix()
+{
 
-	Matrix::Matrix(
-		const float a11, const float a12, const float a13, const float a14,
-		const float a21, const float a22, const float a23, const float a24,
-		const float a31, const float a32, const float a33, const float a34,
-		const float a41, const float a42, const float a43, const float a44) :
-		matrix()
-	{
-		m11 = a11;
-		m12 = a12;
-		m13 = a13;
-		m14 = a14;
-		m21 = a21;
-		m22 = a22;
-		m23 = a23;
-		m24 = a24;
-		m31 = a31;
-		m32 = a32;
-		m33 = a33;
-		m34 = a34;
-		m41 = a41;
-		m42 = a42;
-		m43 = a43;
-		m44 = a44;
-	}
+}
 
-	Matrix::~Matrix()
-	{
+Matrix::Matrix(
+	const float a11, const float a12, const float a13, const float a14,
+	const float a21, const float a22, const float a23, const float a24,
+	const float a31, const float a32, const float a33, const float a34,
+	const float a41, const float a42, const float a43, const float a44) :
+	matrix()
+{
+	m11 = a11;
+	m12 = a12;
+	m13 = a13;
+	m14 = a14;
+	m21 = a21;
+	m22 = a22;
+	m23 = a23;
+	m24 = a24;
+	m31 = a31;
+	m32 = a32;
+	m33 = a33;
+	m34 = a34;
+	m41 = a41;
+	m42 = a42;
+	m43 = a43;
+	m44 = a44;
+}
 
-	}
+Matrix::~Matrix()
+{
 
-	const Matrix Matrix::Identity(
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+const Matrix Matrix::Identity(
+	1.0f, 0.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f, 0.0f,
+	0.0f, 0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 0.0f, 1.0f);
+
 }
