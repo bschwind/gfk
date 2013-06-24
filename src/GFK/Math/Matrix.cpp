acc9eba94ccab3c0d9dea4305fd2e108f8f81924
Brian Schwind <brianmschwind@gmail.com>
2013-06-24 14:09:07 -0400

    Removing unnecessary initialization call

--- src/GFK/Math/Matrix.cpp
+++ src/GFK/Math/Matrix.cpp
@@ -34,8 +34,7 @@ Matrix::Matrix(
 	float a11, float a12, float a13, float a14,
 	float a21, float a22, float a23, float a24,
 	float a31, float a32, float a33, float a34,
-	float a41, float a42, float a43, float a44) :
-	matrix()
+	float a41, float a42, float a43, float a44)
 {
 	m11 = a11;
 	m12 = a12;
