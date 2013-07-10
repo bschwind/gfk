c39601dc28ccb1c399ee78208a8c866a949b0bdd
Brian Schwind <brianmschwind@gmail.com>
2013-07-09 22:22:06 -0400

    Enable OpenGL Core Profile 3.2 and re-add the shader test

--- src/GFK/Graphics/GraphicsDevice.cpp
+++ src/GFK/Graphics/GraphicsDevice.cpp
@@ -3,73 +3,74 @@
 #include <GFK/Graphics/PackedColor.hpp>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <GFK/Graphics/Shader.hpp>
 #include <iostream>
 
 namespace gfk
 {
 
-// // Render scene
-// void display(GLuint &vao);
+// Render scene
+void display(GLuint &vao);
 
-// // Initialize the data to be rendered
-// void initialize(GLuint &vao);
+// Initialize the data to be rendered
+void initialize(GLuint &vao);
 
-// GLuint vao;
+GLuint vao;
 
-// // Render scene
-// void display(GLuint &vao) {
-// 	glClear(GL_COLOR_BUFFER_BIT);
+// Render scene
+void display(GLuint &vao) {
+	glClear(GL_COLOR_BUFFER_BIT);
 
-// 	glBindVertexArray(vao);
-// 	glDrawArrays(GL_TRIANGLES, 0, 12);
-// }
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 12);
+}
 
-// void initialize(GLuint &vao) {
-// 	// Use a Vertex Array Object
-// 	glGenVertexArrays(1, &vao);
-// 	glBindVertexArray(vao);
+void initialize(GLuint &vao) {
+	// Use a Vertex Array Object
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-// 	// 4 triangles to be rendered
-// 	GLfloat vertices_position[24] = {
-// 		0.0, 0.0,
-// 		0.5, 0.0,
-// 		0.5, 0.5,
+	// 4 triangles to be rendered
+	GLfloat vertices_position[24] = {
+		0.0, 0.0,
+		0.5, 0.0,
+		0.5, 0.5,
 
-// 		0.0, 0.0,
-// 		0.0, 0.5,
-// 		-0.5, 0.5,
+		0.0, 0.0,
+		0.0, 0.5,
+		-0.5, 0.5,
 
-// 		0.0, 0.0,
-// 		-0.5, 0.0,
-// 		-0.5, -0.5,		
+		0.0, 0.0,
+		-0.5, 0.0,
+		-0.5, -0.5,		
 
-// 		0.0, 0.0,
-// 		0.0, -0.5,
-// 		0.5, -0.5,
-// 	};
+		0.0, 0.0,
+		0.0, -0.5,
+		0.5, -0.5,
+	};
 
-// 	// Create a Vector Buffer Object that will store the vertices on video memory
-// 	GLuint vbo;
-// 	glGenBuffers(1, &vbo);
+	// Create a Vector Buffer Object that will store the vertices on video memory
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
 
-// 	// Allocate space and upload the data from CPU to GPU
-// 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-// 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_position), vertices_position, GL_STATIC_DRAW);	
+	// Allocate space and upload the data from CPU to GPU
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_position), vertices_position, GL_STATIC_DRAW);	
 
-// 	//GLuint shaderProgram = create_program("vertShader.vs", "fragShader.fs");
+	//GLuint shaderProgram = create_program("vertShader.vs", "fragShader.fs");
 
-// 	Shader shader = Shader::CreateFromFile("vertShader.vs", "fragShader.fs");
+	Shader shader = Shader::CreateFromFile("vertShader.vs", "fragShader.fs");
 
-// 	// Get the location of the attributes that enters in the vertex shader
-// 	GLint position_attribute = glGetAttribLocation(shader.GetGLShaderID(), "position");
+	// Get the location of the attributes that enters in the vertex shader
+	GLint position_attribute = glGetAttribLocation(shader.GetGLShaderID(), "position");
 
-// 	// Specify how the data for position can be accessed
-// 	glVertexAttribPointer(position_attribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	// Specify how the data for position can be accessed
+	glVertexAttribPointer(position_attribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-// 	// Enable the attribute
-// 	glEnableVertexAttribArray(position_attribute);
-// 	std::cout << "Initialized VAO" << std::endl;
-// }
+	// Enable the attribute
+	glEnableVertexAttribArray(position_attribute);
+	std::cout << "Initialized VAO" << std::endl;
+}
 
 GraphicsDevice::GraphicsDevice()
 {
@@ -97,6 +98,12 @@ void GraphicsDevice::Initialize(const std::string &title, int width, int height)
 		exit(EXIT_FAILURE);
 	}
 
+	// Use OpenGL 3.2 core profile
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+
 	window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
 
 	if (!window)
@@ -117,7 +124,7 @@ void GraphicsDevice::Initialize(const std::string &title, int width, int height)
 
 	std::cout << "Initialized GLEW" << std::endl;
 
-	// initialize(vao);
+	initialize(vao);
 }
 
 void GraphicsDevice::SetClearColor(const gfk::Color &color)
@@ -138,6 +145,7 @@ void GraphicsDevice::SetDepthClearValue(float depth)
 void GraphicsDevice::Clear()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	display(vao);
 }
 
 void GraphicsDevice::ClearDepth()
