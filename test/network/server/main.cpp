5712059bc9ef364a33def06c7baa86a942cc5880
Brian Schwind <brianmschwind@gmail.com>
2013-08-12 20:07:06 -0400

    Broke up the test folder into separate test projects

---
+++ test/network/server/main.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <iostream>
+#include <GFK/Game.hpp>
+#include <GFK/Network/UDPSocket.hpp>
+
+using namespace gfk;
+
+class ServerGame : public gfk::Game
+{
+public:
+	ServerGame() : Game(true) {}
+
+	UDPSocket socket;
+	unsigned char receiveBuffer[256];
+
+	void LoadContent()
+	{
+		socket.Open(55777);
+	}
+
+	void Update(const GameTime &gameTime)
+	{
+		IPAddress sender;
+        int byteReadCount = socket.Receive(sender, receiveBuffer, sizeof(receiveBuffer));
+        if (!byteReadCount)
+        {
+            return;
+        }
+
+        std::cout << receiveBuffer << std::endl;
+	}
+};
+
+int main()
+{
+	ServerGame game;
+	game.Run();
+	return 0;
+}
