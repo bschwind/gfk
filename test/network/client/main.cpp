e6f2764a0b588b287e41064af27a46fe3617a16d
Brian Schwind <brianmschwind@gmail.com>
2013-09-11 22:34:25 -0400

    Breaking the library out into smaller pieces: math, network, graphics, gfk

--- test/network/client/main.cpp
+++ test/network/client/main.cpp
@@ -1,36 +1,25 @@
 #include <string>
 #include <iostream>
-#include <GFK/Game.hpp>
 #include <GFK/Network/UDPSocket.hpp>
 
 using namespace gfk;
 
-class ClientGame : public gfk::Game
+int main()
 {
-public:
-	ClientGame() : Game(true) {}
-
 	UDPSocket socket;
 	static const int length = 256;
 
-	void LoadContent()
-	{
-		socket.Open(55778);
-	}
+	socket.Open(55778);
 
-	void Update(const GameTime &gameTime)
+	std::string message;
+
+	while (true)
 	{
-		std::string message;
 		std::getline(std::cin, message);
 		char *sendBuffer = (char*)message.c_str();
 
 		socket.Send(IPAddress(127,0,0,1,55777), sendBuffer, length);
 	}
-};
 
-int main()
-{
-	ClientGame game;
-	game.Run();
 	return 0;
 }
--- test/network/server/main.cpp
+++ test/network/server/main.cpp
@@ -1,39 +1,27 @@
 #include <string>
 #include <iostream>
-#include <GFK/Game.hpp>
 #include <GFK/Network/UDPSocket.hpp>
 
 using namespace gfk;
 
-class ServerGame : public gfk::Game
+int main()
 {
-public:
-	ServerGame() : Game(true) {}
-
 	UDPSocket socket;
 	unsigned char receiveBuffer[256];
 
-	void LoadContent()
-	{
-		socket.Open(55777);
-	}
+	socket.Open(55777);
 
-	void Update(const GameTime &gameTime)
+	IPAddress sender;
+	while (true)
 	{
-		IPAddress sender;
 		int byteReadCount = socket.Receive(sender, receiveBuffer, sizeof(receiveBuffer));
 		if (!byteReadCount)
 		{
-			return;
+			continue;
 		}
 
 		std::cout << receiveBuffer << std::endl;
 	}
-};
 
-int main()
-{
-	ServerGame game;
-	game.Run();
 	return 0;
 }
