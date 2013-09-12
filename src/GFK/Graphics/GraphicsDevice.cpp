53698ae0c65039229c7adb8f93e3a7df475a4b76
Brian Schwind <brianmschwind@gmail.com>
2013-09-11 22:56:30 -0400

    For the time being, comment out the OpenGL vertex buffer test code

--- src/GFK/Graphics/GraphicsDevice.cpp
+++ src/GFK/Graphics/GraphicsDevice.cpp
@@ -8,63 +8,63 @@
 namespace gfk
 {
 
-// Render scene
-void display(GLuint &vao);
+// // Render scene
+// void display(GLuint &vao);
 
-// Initialize the data to be rendered
-void initialize(GLuint &vao);
+// // Initialize the data to be rendered
+// void initialize(GLuint &vao);
 
-GLuint vao;
+// GLuint vao;
 
-// Render scene
-void display(GLuint &vao) {
-	glBindVertexArray(vao);
-	glDrawArrays(GL_TRIANGLES, 0, 12);
-}
+// // Render scene
+// void display(GLuint &vao) {
+// 	glBindVertexArray(vao);
+// 	glDrawArrays(GL_TRIANGLES, 0, 12);
+// }
 
-void initialize(GLuint &vao) {
-	// Use a Vertex Array Object
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+// void initialize(GLuint &vao) {
+// 	// Use a Vertex Array Object
+// 	glGenVertexArrays(1, &vao);
+// 	glBindVertexArray(vao);
 
-	// 4 triangles to be rendered
-	GLfloat vertices_position[24] = {
-		0.0, 0.0,
-		0.5, 0.0,
-		0.5, 0.5,
+// 	// 4 triangles to be rendered
+// 	GLfloat vertices_position[24] = {
+// 		0.0, 0.0,
+// 		0.5, 0.0,
+// 		0.5, 0.5,
 
-		0.0, 0.0,
-		0.0, 0.5,
-		-0.5, 0.5,
+// 		0.0, 0.0,
+// 		0.0, 0.5,
+// 		-0.5, 0.5,
 
-		0.0, 0.0,
-		-0.5, 0.0,
-		-0.5, -0.5,		
+// 		0.0, 0.0,
+// 		-0.5, 0.0,
+// 		-0.5, -0.5,		
 
-		0.0, 0.0,
-		0.0, -0.5,
-		0.5, -0.5,
-	};
+// 		0.0, 0.0,
+// 		0.0, -0.5,
+// 		0.5, -0.5,
+// 	};
 
-	// Create a Vector Buffer Object that will store the vertices on video memory
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
+// 	// Create a Vector Buffer Object that will store the vertices on video memory
+// 	GLuint vbo;
+// 	glGenBuffers(1, &vbo);
 
-	// Allocate space and upload the data from CPU to GPU
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_position), vertices_position, GL_STATIC_DRAW);
+// 	// Allocate space and upload the data from CPU to GPU
+// 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+// 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_position), vertices_position, GL_STATIC_DRAW);
 
-	Shader shader = Shader::CreateFromFile("vertShader.vs", "fragShader.fs");
+// 	Shader shader = Shader::CreateFromFile("vertShader.vs", "fragShader.fs");
 
-	// Get the location of the attributes that enter into the vertex shader
-	GLint position_attribute = glGetAttribLocation(shader.Natives.OpenGL.ShaderID, "position");
+// 	// Get the location of the attributes that enter into the vertex shader
+// 	GLint position_attribute = glGetAttribLocation(shader.Natives.OpenGL.ShaderID, "position");
 
-	// Specify how the data for position can be accessed
-	glVertexAttribPointer(position_attribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
+// 	// Specify how the data for position can be accessed
+// 	glVertexAttribPointer(position_attribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-	// Enable the attribute
-	glEnableVertexAttribArray(position_attribute);
-}
+// 	// Enable the attribute
+// 	glEnableVertexAttribArray(position_attribute);
+// }
 
 GraphicsDevice::GraphicsDevice() :
 gameShouldClose(false)
@@ -96,7 +96,7 @@ void GraphicsDevice::Initialize()
 
 	std::cout << "Initialized GLEW" << std::endl;
 
-	initialize(vao);
+	// initialize(vao);
 
 	//glfwSwapInterval(0); - 0 for no VSync, 1 for VSync
 }
@@ -207,7 +207,7 @@ void GraphicsDevice::Clear()
 	{
 		glfwMakeContextCurrent(*iter);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		display(vao);
+		// display(vao);
 	}
 }
 
