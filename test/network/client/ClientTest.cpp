434972a0fa7dcddb295ed500f6166483bdc324c5
Brian Schwind <brianmschwind@gmail.com>
2013-10-02 22:01:30 -0400

    Changed the client test to talk to an 'echo' server

--- test/network/client/ClientTest.cpp
+++ test/network/client/ClientTest.cpp
@@ -4,23 +4,42 @@
 
 using namespace gfk;
 
-int main()
+int main(int argc, char* argv[])
 {
+	int portInt = atoi(argv[1]);
+	unsigned short port = (unsigned short)portInt;
+
 	UDPSocket::InitializeSocketLayer();
 
 	UDPSocket socket;
 	static const int length = 256;
 
-	socket.Open(55778);
+	socket.Open(port);
 
 	std::string message;
+
+	unsigned char receiveBuffer[256];
+	IPAddress sender;
 
 	while (true)
 	{
 		std::getline(std::cin, message);
 		char *sendBuffer = (char*)message.c_str();
 
-		socket.Send(IPAddress(192,168,1,6,55777), sendBuffer, length);
+		socket.Send(IPAddress(127,0,0,1,55777), sendBuffer, length);
+
+		while (true)
+		{
+			int byteReadCount = socket.Receive(sender, receiveBuffer, sizeof(receiveBuffer));
+
+			if (!byteReadCount)
+			{
+				continue;
+			}
+
+			std::cout << receiveBuffer << std::endl;
+			break;
+		}
 	}
 
 	UDPSocket::ShutdownSocketLayer();
