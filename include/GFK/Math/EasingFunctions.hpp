6ba12e76df007f6d6a7a502aead13214b0d3b130
Brian Schwind <brianmschwind@gmail.com>
2014-04-12 21:18:17 +0900

    Added animation easing functions

---
+++ include/GFK/Math/EasingFunctions.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+namespace gfk
+{
+
+class EasingFunctions
+{
+
+public:
+	// t: current time, b: beginning value, c: change in value, d: duration
+	static float easeInBack(float t, float b, float c, float d);
+	static float easeOutBack(float t, float b, float c, float d);
+	static float easeInOutBack(float t, float b, float c, float d);
+
+	static float easeInBounce(float t, float b, float c, float d);
+	static float easeOutBounce(float t, float b, float c, float d);
+	static float easeInOutBounce(float t, float b, float c, float d);
+
+	static float easeInCirc(float t, float b, float c, float d);
+	static float easeOutCirc(float t, float b, float c, float d);
+	static float easeInOutCirc(float t, float b, float c, float d);
+
+	static float easeInCubic(float t, float b, float c, float d);
+	static float easeOutCubic(float t, float b, float c, float d);
+	static float easeInOutCubic(float t, float b, float c, float d);
+
+	static float easeInElastic(float t, float b, float c, float d);
+	static float easeOutElastic(float t, float b, float c, float d);
+	static float easeInOutElastic(float t, float b, float c, float d);
+
+	static float easeInExpo(float t, float b, float c, float d);
+	static float easeOutExpo(float t, float b, float c, float d);
+	static float easeInOutExpo(float t, float b, float c, float d);
+
+	static float easeInLinear(float t, float b, float c, float d);
+	static float easeOutLinear(float t, float b, float c, float d);
+	static float easeInOutLinear(float t, float b, float c, float d);
+
+	static float easeInQuad(float t, float b, float c, float d);
+	static float easeOutQuad(float t, float b, float c, float d);
+	static float easeInOutQuad(float t, float b, float c, float d);
+
+	static float easeInQuart(float t, float b, float c, float d);
+	static float easeOutQuart(float t, float b, float c, float d);
+	static float easeInOutQuart(float t, float b, float c, float d);
+
+	static float easeInQuint(float t, float b, float c, float d);
+	static float easeOutQuint(float t, float b, float c, float d);
+	static float easeInOutQuint(float t, float b, float c, float d);
+
+	static float easeInSine(float t, float b, float c, float d);
+	static float easeOutSine(float t, float b, float c, float d);
+	static float easeInOutSine(float t, float b, float c, float d);
+};
+
+}
