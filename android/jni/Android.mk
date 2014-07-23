e83fedf93f6c7f2f5e8e7cbbd238f2722904df51
Brian Schwind <brianmschwind@gmail.com>
2014-07-23 13:39:04 -0400

    Remove assimp from the android project for now

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -4,15 +4,6 @@ GFK_PATH := $(LOCAL_PATH)/../..
 #LOCAL_SRC_FILES is relative to the jni directory
 GFK_SRC := ../../src
 
-# AssImp
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libassimp
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libassimp.so
-LOCAL_EXPORT_C_INCLUDES := $(GFK_PATH)/lib/assimp/include
-
-include $(PREBUILT_SHARED_LIBRARY)
-
 # GFK
 include $(CLEAR_VARS)
 
@@ -27,7 +18,6 @@ LOCAL_SRC_FILES := \
 	$(GFK_SRC)/GFK/Math/Vector2.cpp \
 	$(GFK_SRC)/GFK/Math/MathHelper.cpp \
 	$(GFK_SRC)/GFK/Network/IPAddress.cpp \
-	$(GFK_SRC)/GFK/GameTime.cpp \
 	$(GFK_SRC)/GFK/Game.cpp \
 	$(GFK_SRC)/GFK/Network/UDPSocket.cpp \
 	$(GFK_SRC)/GFK/Network/BytePacker.cpp \
@@ -44,9 +34,8 @@ LOCAL_SRC_FILES := \
 	$(GFK_SRC)/GFK/Graphics/Shader.cpp \
 	$(GFK_SRC)/GFK/Graphics/PrimitiveBatch2D.cpp \
 	$(GFK_SRC)/GFK/Graphics/PrimitiveBatch3D.cpp \
-	$(GFK_SRC)/GFK/Graphics/AssImpBridge.cpp \
-	$(GFK_SRC)/GFK/Graphics/Mesh.cpp \
 	$(GFK_SRC)/GFK/Graphics/GLErrorHandling.cpp \
+	$(GFK_SRC)/GFK/System/GameTime.cpp \
 	$(GFK_SRC)/GFK/System/Logger.cpp \
 
 LOCAL_LDLIBS    := -llog -lGLESv2
