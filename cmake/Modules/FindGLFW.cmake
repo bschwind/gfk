488439064b04383d02da96948cc640f60c92549b
Brian Schwind <brianmschwind@gmail.com>
2014-11-08 18:59:16 -0500

    Adding the static versions of GLFW and Assimp

---
+++ cmake/Modules/FindGLFW.cmake
@@ -0,0 +1,28 @@
+#
+# Try to find GLFW library and include path.
+# Once done this will define
+#
+# GLFW_FOUND
+# GLFW_INCLUDE_PATH
+# GLFW_LIBRARY
+
+FIND_PATH(GLFW_INCLUDE_PATH GLFW/glfw3.h
+	${PROJECT_SOURCE_DIR}/lib/glfw/include/
+)
+
+FIND_LIBRARY(GLFW_LIBRARY
+	NAMES
+		libglfw3.a # OS X
+	PATHS
+		${PROJECT_SOURCE_DIR}/lib/glfw/osx/
+)
+
+SET(GLFW_FOUND "NO")
+IF (GLFW_INCLUDE_PATH AND GLFW_LIBRARY)
+	SET(GLFW_INCLUDE_PATH ${GLFW_INCLUDE_PATH})
+	SET(GLFW_LIBRARY ${GLFW_LIBRARY})
+	SET(GLFW_FOUND "YES")
+ENDIF (GLFW_INCLUDE_PATH AND GLFW_LIBRARY)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GLFW DEFAULT_MSG GLFW_LIBRARY GLFW_INCLUDE_PATH)
